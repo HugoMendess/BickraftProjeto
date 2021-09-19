f8c9a20a58cf1bfda4d3f9c29be9d09fdb408de4
Hugo <hugomendes@aluno.uema.br>
2021-09-19 12:49:56 -0300

    Finalizando as animações com css e javascript

--- js/main.js
+++ js/main.js
@@ -12,3 +12,29 @@ $(".rslides_portolio").responsiveSlides({
   pager: true,           // Boolean: Show pager, true or false          // Boolean: Show navigation, true or false
    // Function: After callback
 });
+
+
+Visibility.onVisible(function(){
+	setTimeout(function () {
+		$(".introducao h1").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".introducao blockquote").addClass("animated fadeInDown");
+	}, 800);
+	setTimeout(function () {
+		$(".introducao .btn").addClass("animated fadeInDown");
+	}, 1200);
+	setTimeout(function () {
+		$(".animar").addClass("animated fadeInDown");
+	}, 1600);
+
+	setTimeout(function () {
+		$(".introducao-interna h1").addClass("animated fadeInDown");
+	}, 400);
+	setTimeout(function () {
+		$(".introducao-interna p").addClass("animated fadeInDown");
+	}, 800);
+	setTimeout(function () {
+		$(".animar-interno").addClass("animated fadeInDown");
+	}, 1200);
+});
