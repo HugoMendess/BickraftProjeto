4a644b7047351d0340271a90ba28bbd356da5b8b
Hugo <hugomendes@aluno.uema.br>
2021-09-19 11:52:28 -0300

    Começando a trabalhar com plugins

--- js/main.js
+++ js/main.js
@@ -1,7 +1,14 @@
+$(".rslides").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
 
+});
 
-  $(".rslides").responsiveSlides({
-      auto: true,
-      speed: 500,
-      timeout: 1000,
-  });
+$(".rslides_portolio").responsiveSlides({
+  auto: true,             // Boolean: Animate automatically, true or false
+  speed: 500,            // Integer: Speed of the transition, in milliseconds
+  timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
+  pager: true,           // Boolean: Show pager, true or false          // Boolean: Show navigation, true or false
+   // Function: After callback
+});
